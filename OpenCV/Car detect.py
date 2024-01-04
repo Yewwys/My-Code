39594633a39a8f5a1af055623265ebc0a12c3519
YewIthipunkul <139699878+Yewwys@users.noreply.github.com>
2024-01-04 23:07:48 +0700

    Update

--- OpenCV/Car detect.py
+++ OpenCV/Car detect.py
@@ -1,6 +1,6 @@
 import cv2
 ################################################
-path = 'Resources/licence-plate-autcion-b4fd.jpg'
+path = 'Resources/929055_GErCUWjGo4Z0Lj1WsoK8NS.jpg'
 nplateCascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
 count = 0
 minArea = 1000
@@ -14,9 +14,15 @@ while True:
     for (x,y,w,h) in numberPlate:
         area = w*h 
         if area>minArea:
-            cv2.rectangle(img,(x,y-30),x+w+30,y+h,(0,0,255),2)
+            cv2.rectangle(img,(x,y-30),(x+w+30,y+h),(0,0,255),2)
             cv2.putText(img,"NUMBER",(x,y-35),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,255,0),2)
             imgRoi = img[y-30:y+h,x:x+w+30]
             cv2.imshow("ROI",imgRoi)
             cv2.imshow("Original",img)
-            cv2.waitKey(500)        
+    if cv2.waitKey(500) & 0xFF == ord('s'):
+        cv2.imwrite("Resources/NumPlate"+str(count)+".jpg",imgRoi) 
+        cv2.putText(img,"Saved",(50,50),cv2.FONT_HERSHEY_COMPLEX_DUPLEX,2,(0,0,255),2)
+        count += 1
+        cv2.imshow('SAVED',img)
+        cv2.waitKey(500)     
+    
